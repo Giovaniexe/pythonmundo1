04b63762d2692a78292d17e627d754268efca088
Giovaniexe <104697210+Giovaniexe@users.noreply.github.com>
2022-05-25 13:55:29 -0300

    Update aluno_aleatorio.py

--- aluno_aleatorio.py
+++ aluno_aleatorio.py
@@ -1,11 +1,23 @@
-import random
+from random import choice, shuffle
 
 print('Um professor quer sortear um dos seus 4 alunos para apagar o quadro.'
       'faça um programa que ajude ele, lendo o nomede deles e escrevendo o nome do escolhido')
 
-nomes = random.randint(1, 4)
-1 = str('Tauanê')
+n1 = str(input('Digite o nome do primeiro aluno '))
+n2 = str(input('Digite o nome do segundo aluno '))
+n3 = str(input('Digite o nome do terceiro aluno '))
+n4 = str(input('Digite o nome do quarto aluno '))
 
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O aluno escolhido foi: {}'.format(escolhido))
+
+print('__________________________________________________')
+
+print('o mesmo professor agora deseja sortear a ordem'
+      'de apresentação dos trabalhos dos 4 alunos, escreva o programa')
+shuffle(lista)
+print('A ordem de apresentação será: {}'.format(lista))
 
 
 
